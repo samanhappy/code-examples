7078ad3ab70d24f8930ce8252cd3d928360d7e0b
samanhappy <samanhappy@gmail.com>
2015-09-22 13:44:28 +0800

    remove springside test profile

--- spring-data-jpa/src/test/java/org/samanhappy/spring/SpringTransactionalTestCase.java
+++ spring-data-jpa/src/test/java/org/samanhappy/spring/SpringTransactionalTestCase.java
@@ -3,12 +3,12 @@ package org.samanhappy.spring;
 import javax.sql.DataSource;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.test.context.ActiveProfiles;
+//import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.junit4.AbstractTransactionalJUnit4SpringContextTests;
-import org.springside.modules.test.spring.Profiles;
+//import org.springside.modules.test.spring.Profiles;
 
 // springside支持Profiles设定
-@ActiveProfiles(Profiles.UNIT_TEST)
+//@ActiveProfiles(Profiles.UNIT_TEST)
 public abstract class SpringTransactionalTestCase extends AbstractTransactionalJUnit4SpringContextTests{
 	
 	protected DataSource dataSource;
